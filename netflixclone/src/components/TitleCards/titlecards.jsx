a1c6ef46ee3f5858e9c7cb6521b0e6b1b43a0dc7
ayesha942 <munirayesha009@gmail.com>
2025-06-09 13:34:52 +0500

    added titlecards

--- netflixclone/src/components/TitleCards/titlecards.jsx
+++ netflixclone/src/components/TitleCards/titlecards.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import './titlecards.css'
-const titlecards = () => {
+import React from 'react';
+import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
+
+const TitleCards = () => {
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className='card-list'>
+        {cards_data.map((card, index) => {
+          return (
+            <div className='card' key={index}>
+              <img src={card.image} alt={`Card ${index}`} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default titlecards
+export default TitleCards;
--- netflixclone/src/pages/Home/Home.jsx
+++ netflixclone/src/pages/Home/Home.jsx
@@ -3,14 +3,21 @@ import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/Play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
       <div className='hero'>
-    <img src={hero_banner} alt='banner' className='banner-imae'></img>
+    <img src={hero_banner} alt='banner' className='banner-image'></img>
     <div className='hero-caption'>
         <img src={hero_title} alt='herotitle' className='caption-image'></img>
+        <p>Discovering his ties to  secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from immortal enemy</p>
+        <div className='hero-btns'>
+          <button className='btn'><img src={play_icon}></img>Play</button>
+           <button className='btn dark-btn'><img src={info_icon}></img>More Info</button>
+        </div>
     </div>
       </div>
     </div>
