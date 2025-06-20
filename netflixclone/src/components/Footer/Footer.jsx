d1656e56fa645407ca84255f638841c843551176
ayesha942 <munirayesha009@gmail.com>
2025-06-20 19:18:11 +0500

    added - footer

--- netflixclone/src/components/Footer/Footer.jsx
+++ netflixclone/src/components/Footer/Footer.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import './Footer.css'
-import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assets/instagram_icon.png'
-import facebook_icon from '../../assets/facebook_icon.png'
-const footer = () => {
-  return (
-    <div className='Footer'>
-      <div className="footer-cons">
-        <img src={facebook_icon}></img>
-         <img src={instagram_icon}></img>
-          <img src={youtube_icon}></img>
-           <img src={twitter_icon}></img>
-      </div>
-    </div>
-  )
-}
+// src/components/Footer/Footer.jsx
+import React from 'react';
+import './Footer.css';
+import youtube_icon from '../../assets/youtube_icon.png';
+import twitter_icon from '../../assets/twitter_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import facebook_icon from '../../assets/facebook_icon.png';
 
-export default Footer
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="Facebook" />
+        <img src={instagram_icon} alt="Instagram" />
+        <img src={youtube_icon} alt="YouTube" />
+        <img src={twitter_icon} alt="Twitter" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre </li>
+        <li>Investor Relations</li>
+        <li>Jobs </li>
+        <li>Terms of use </li>
+        <li>Privacy </li>
+        <li>Legal Notice </li>
+        <li>Cookie Preferences </li>
+        <li>Corporate Information </li>
+         <li>Contact us </li>
+      </ul>
+      <p className='copyright-text'>
+        All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- netflixclone/src/components/TitleCards/titlecards.jsx
+++ netflixclone/src/components/TitleCards/titlecards.jsx
@@ -1,15 +1,15 @@
+// src/components/TitleCards/TitleCards.jsx
 import React, { useEffect, useRef } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
-const TitleCards = ({title,category}) => {
-
+const TitleCards = ({ title }) => {
   const cardsRef = useRef();
 
   const handleWheel = (event) => {
     event.preventDefault();
     if (cardsRef.current) {
-      cardsRef.current.scrollLeft += event.deltaY; // Use correct property
+      cardsRef.current.scrollLeft += event.deltaY;
     }
   };
 
@@ -18,8 +18,6 @@ const TitleCards = ({title,category}) => {
     if (ref) {
       ref.addEventListener('wheel', handleWheel);
     }
-
-    // Clean up
     return () => {
       if (ref) {
         ref.removeEventListener('wheel', handleWheel);
@@ -29,7 +27,7 @@ const TitleCards = ({title,category}) => {
 
   return (
     <div className='titlecards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title || "Popular on Netflix"}</h2>
       <div className='card-list' ref={cardsRef}>
         {cards_data.map((card, index) => (
           <div className='card' key={index}>
