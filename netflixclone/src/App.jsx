f048610b19c594ed24dae64438685411553817bd
ayesha942 <munirayesha009@gmail.com>
2025-06-20 19:27:25 +0500

    added - routes

--- netflixclone/src/App.jsx
+++ netflixclone/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './pages/Home/Home';
-
+import { Routes,Route } from 'react-router-dom';
+import Login from './pages/Login/login'
 const App = () => {
   return (
     <div>
-      <Home /> {/* Use capital 'H' here too */}
+      <Routes>
+        <Route path='/'element={ <Home />}></Route>
+          <Route path='/login'element={ <Login />}></Route>
+      </Routes>
+      {/* Use capital 'H' here too */}
     </div>
   );
 };
